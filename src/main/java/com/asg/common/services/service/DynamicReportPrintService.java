980613f3b0398aaef04494ef8a00330f1ca3c609
Sahil Khurana <skhurana@hexalytics.com>
2026-02-10 01:27:46 +0530

    Dynamic Report Fix

--- src/main/java/com/asg/common/services/service/DynamicReportPrintService.java
+++ src/main/java/com/asg/common/services/service/DynamicReportPrintService.java
@@ -79,14 +79,90 @@ public class DynamicReportPrintService {
     
     private JasperReport getReportConfig(String docId, Map<String, Object> params) throws Exception {
         return switch (docId) {
+            case "100-292" -> printService.load("DynamicReport/SH/Ship_LineWise_Inventory_Daily.jrxml");
+            case "100-293" -> printService.load("DynamicReport/SH/Ship_Linewise_Transhipment.jrxml");
+            case "100-360" -> printService.load("DynamicReport/RevenueReports/ShippingRevenueLineContainerComparisonReport.jrxml");
+            case "100-361" -> {
+                params.put("SUBREPORT1", printService.load("DynamicReport/RevenueReports/ShippingRevenueLineComparisonSubreport.jrxml"));
+                yield printService.load("DynamicReport/RevenueReports/ShippingRevenueLineComparisonReport.jrxml");
+            }
+            case "100-396" -> printService.load("DynamicReport/SH/SH_EXPORT_DAYBOOK_MONTHLY.jrxml");
+            case "100-463", "100-473" -> {
+                params.put("SUBREPORT1", printService.load("DynamicReport/SALES/Shipping_Charges_Subreport.jrxml"));
+                params.put("SUBREPORT2", printService.load("DynamicReport/SALES/Shipping_Demurrage_SubReport.jrxml"));
+                params.put("SUBREPORT3", printService.load("DynamicReport/SALES/Shipping_Charges_Export_Subreport.jrxml"));
+                params.put("SUBREPORT4", printService.load("DynamicReport/SALES/Shipping_Detention_SubReport.jrxml"));
+                params.put("SUBREPORT5", printService.load("DynamicReport/SALES/Shipping_Charges_Final_Dummy.jrxml"));
+                yield printService.load("DynamicReport/SALES/Shipping_Charges_List.jrxml");
+            }
+            case "300-203" -> printService.load("DynamicReport/GL/CustomerLedgerStatement_A4.jrxml");
+            case "400-201" -> {
+                params.put("SUBREPORT1", printService.load("DynamicReport/GL/TrialBalanceReportSubreport1.jrxml"));
+                yield printService.load("DynamicReport/GL/TrialBalanceReport.jrxml");
+            }
+            case "400-202" -> {
+                params.put("SUBREPORT1", printService.load("DynamicReport/GL/ProfitandLossReportSubreport1.jrxml"));
+                yield printService.load("DynamicReport/GL/ProfitandLossReport.jrxml");
+            }
             case "400-205" -> {
                 params.put("SUB_CONTACT", printService.load("DynamicReport/GL/BillwiseLedgerStatement_Contact_subreport1.jrxml"));
                 yield printService.load("DynamicReport/GL/BillwiseLedgerStatement_A4.jrxml");
             }
+            case "400-219" -> {
+                params.put("SUBREPORT1", printService.load("DynamicReport/GL/LedgerGroupSummarySubReport1.jrxml"));
+                yield printService.load("DynamicReport/Gl/LedgerGroupSummary.jrxml");
+            }
+            case "400-240" -> printService.load("DynamicReport/GL/LedgerStatementFC.jrxml");
+            case "400-331" -> {
+                params.put("SUBREPORT1", printService.load("DynamicReport/GL/DivisionWiseComparisonSubreport.jrxml"));
+                yield printService.load("DynamicReport/GL/DivisionWiseComparisonReport.jrxml");
+            }
+            case "400-332" -> printService.load("DynamicReport/RevenueReports/PropertiesMonthlyRentSummaryReport.jrxml");
+            case "400-343" -> printService.load("DynamicReport/RevenueReports/ShippingRevenueYearWiseQtyComparisonReport.jrxml");
             case "400-347" -> {
                 params.put("SUB_CONTACT", printService.load("DynamicReport/GL/BillwiseLedgerStatement_Contact_subreport1.jrxml"));
                 yield printService.load("DynamicReport/GL/BillwiseLedgerStatementFC_A4.jrxml");
             }
+            case "400-356" -> {
+                params.put("SUBREPORT1", printService.load("DynamicReport/RevenueReports/DivisionWiseComparisonSubreport.jrxml"));
+                yield printService.load("DynamicReport/RevenueReports/DivisionWiseComparisonReport.jrxml");
+            }
+            case "400-357" -> {
+                params.put("SUBREPORT1", printService.load("DynamicReport/RevenueReports/ShippingRevenueLineComparisonSubreport.jrxml"));
+                yield printService.load("DynamicReport/RevenueReports/ShippingRevenueLineComparisonReport.jrxml");
+            }
+            case "400-365" -> {
+                params.put("SUBREPORT1", printService.load("DynamicReport/RevenueReports/OverAllGLLedgerDivisionWiseSubreport.jrxml"));
+                yield printService.load("DynamicReport/RevenueReports/OverAllGLLedgerDivisionWiseReport.jrxml");
+            }
+            case "400-375", "400-377", "400-378", "400-379", "400-380", "400-381", "400-382", "400-384", "400-385", "400-386", "400-387", "400-388", "400-392" -> {
+                params.put("SUBREPORT1", printService.load("DynamicReport/GL/TrialBalanceReportSubreport1.jrxml"));
+                yield printService.load("DynamicReport/GL/TrialBalanceReport.jrxml");
+            }
+            case "400-389" -> {
+                params.put("SUBREPORT1", printService.load("DynamicReport/GL/TRIAL_BALANCE/TrialBalanceReportSubreport1.jrxml"));
+                yield printService.load("DynamicReport/GL/TRIAL_BALANCE/TrialBalanceReport.jrxml");
+            }
+            case "400-396" -> {
+                params.put("SUBREPORT1", printService.load("DynamicReport/GL/TradeDebtorsSummarySubReport1.jrxml"));
+                yield printService.load("DynamicReport/GL/TradeDebtorsSummary.jrxml");
+            }
+            case "400-406" -> {
+                params.put("SUBREPORT1", printService.load("DynamicReport/RevenueReports/ShippingRevenueLineComparisonSubreport.jrxml"));
+                yield printService.load("DynamicReport/RevenueReports/ShippingRevenueLineComparisonReport.jrxml");
+            }
+            case "400-433" -> {
+                params.put("SUBREPORT1", printService.load("DynamicReport/RevenueReports/DivisionWiseComparisonPeriodSubreport.jrxml"));
+                yield printService.load("DynamicReport/RevenueReports/DivisionWiseComparisonPeriodReport.jrxml");
+            }
+            case "400-434", "400-453" -> {
+                params.put("SUBREPORT1", printService.load("DynamicReport/RevenueReports/ShippingRevenueLineComparisonSubreport.jrxml"));
+                yield printService.load("DynamicReport/RevenueReports/ShippingRevenueLineComparisonReport.jrxml");
+            }
+            case "600-201" -> printService.load("DynamicReport/FA/DepreciationScheduleReport.jrxml");
+            case "600-203" -> printService.load("DynamicReport/FA/CategoryWiseDepreciationScheduleReport.jrxml");
+            case "800-204" -> printService.load("DynamicReport/HR/HrPayrollCustomPrint.jrxml");
+            case "800-219" -> printService.load("DynamicReport/HR/HrProvisionsTillDateRpt.jrxml");
             default -> printService.load("DynamicReport/DynamicReport_A3.jrxml");
         };
     }
